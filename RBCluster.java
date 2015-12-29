86584f919fb7e59899f1b2c09b12b15ccb62b8ba
tan-jerene <jerene.ricketts@gmail.com>
2015-12-29 12:31:18 -0500

    Create RBCluster.java

---
+++ RBCluster.java
@@ -0,0 +1,15 @@
+public class RBCluster{
+	private int numNodes;
+	private boolean critical;
+	private RBNode node;
+
+	public RBCluster(int numNodes, boolean critical){
+		this.critical = critical;
+		this.numNodes = numNodes;
+	}
+
+	public void fixCluster(){
+		if(numNodes == 3)
+				
+	}
+}
